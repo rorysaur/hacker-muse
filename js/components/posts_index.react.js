2a0684c5e288e6406151ff6094827c672213c819
Forrest Thomas <forrestbthomas@gmail.com>
2015-04-27 19:06:03 -0700

    add fixture data for post list

--- js/components/posts_index.react.js
+++ js/components/posts_index.react.js
@@ -1,9 +1,17 @@
 var React = require('react');
+var Fixtures = require('../fixtures/post-data');
 
 var PostsIndex = React.createClass({
   render: function() {
     return (
-      <h1>hi i am a posts index!</h1>
+      <div>
+        // <h1>hi i am a posts index!</h1>
+        //   <ul>
+        //     <li>{Fixtures[0]}</li>
+        //     <li>{Fixtures[1]}</li>
+        //     <li>{Fixtures[2]}</li>
+        //   </ul>
+        </div>
     );
   }
 });
---
+++ js/fixtures/post-data.js
@@ -0,0 +1,19 @@
+var data = [
+    {
+      "title": "A post",
+      "link": "with/a/link.com",
+      "author": "A"
+    },
+    {
+      "title": "B post",
+      "link": "with/b/link.com",
+      "author": "B"
+    },
+    {
+      "title": "C post",
+      "link": "with/c/link.com",
+      "author": "C"
+    }
+  ];
+
+module.exports = data;
