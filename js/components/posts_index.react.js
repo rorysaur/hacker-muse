26a7a184635f03d9fb2748f0a6100440cbffb4ff
Forrest Thomas <forrestbthomas@gmail.com>
2015-04-27 19:17:21 -0700

    uncomment and fix indent

--- js/components/posts_index.react.js
+++ js/components/posts_index.react.js
@@ -4,14 +4,14 @@ var Fixtures = require('../fixtures/post-data');
 var PostsIndex = React.createClass({
   render: function() {
     return (
-      <div>
-        // <h1>hi i am a posts index!</h1>
-        //   <ul>
-        //     <li>{Fixtures[0]}</li>
-        //     <li>{Fixtures[1]}</li>
-        //     <li>{Fixtures[2]}</li>
-        //   </ul>
-        </div>
+    <div>
+      <h1>hi i am a posts index!</h1>
+      <ul>
+        <li>{Fixtures[0]}</li>
+        <li>{Fixtures[1]}</li>
+        <li>{Fixtures[2]}</li>
+      </ul>
+    </div>
     );
   }
 });
