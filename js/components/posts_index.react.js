eeabe74cee4c593aaee145646883bcba88ef6b27
rorysaur <ou.rory@gmail.com>
2015-05-04 18:37:55 -0700

    get posts from store

--- js/components/posts_index.react.js
+++ js/components/posts_index.react.js
@@ -1,20 +1,34 @@
 var React = require('react');
-var Fixtures = require('../fixtures/post-data');
+var PostStore = require('../stores/post_store');
 
 var PostsIndex = React.createClass({
+  componentDidMount: function() {
+    PostStore.on('change', this._handlePostsChange());
+  },
+
+  getInitialState: function() {
+    return {
+      posts: PostStore.all()
+    };
+  },
+
   render: function() {
     return (
     <div>
       <h1>hi i am a posts index!</h1>
       <ul>
       {
-        Fixtures.map(function(item) {
+        this.state.posts.map(function(item) {
           return <li><a href={item.url}>{item.title}</a></li>
         })
       }
       </ul>
     </div>
     );
+  },
+
+  _handlePostsChange: function() {
+    // do it
   }
 });
 
