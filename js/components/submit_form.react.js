54b1a75a9487f868980f210a3717839ab3098b57
Forrest Thomas <forrestbthomas@gmail.com>
2015-05-04 18:35:53 -0700

    add initial submit link handler

--- js/components/submit_form.react.js
+++ js/components/submit_form.react.js
@@ -1,20 +1,29 @@
 var React = require('react');
+var Fixtures = require('../fixtures/post-data');
+var Router = require('react-router');
 
 var SubmitForm = React.createClass({
+  mixins: [Router.Navigation],
+  handleSubmit: function(event) {
+    var url = this.refs.urlRef.getDOMNode().value.trim();
+    var title = this.refs.titleRef.getDOMNode().value.trim();
+    Fixtures.push({url: url, title: title});
+    this.transitionTo('/');
+  },
   render: function() {
     return (
       <div>
         <h1>hi i am a submit form!</h1>
         <div class="input-url">
           <label for="url">url</label>
-          <input type="text" />
+          <input type="text" ref="urlRef"/>
         </div>
         <div class="input-title">
           <label for="title">title</label>
-          <input type="text" />
+          <input type="text" ref="titleRef" />
         </div>
         <div class="submit">
-          <button>submit</button>
+          <button onClick={this.handleSubmit}>submit</button>
         </div>
       </div>
     );
